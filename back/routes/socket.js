e6a8f40ea89cea49b96c616c3796b9b6a3bb6549
Sngwon <dlatmddnjs20@gmail.com>
2021-12-09 03:59:58 +0900

    checkpoint

--- back/routes/socket.js
+++ back/routes/socket.js
@@ -41,15 +41,19 @@ module.exports = io => {
 		socket.on('CHAT_MESSAGE', async msg => {
 			const targetSockets = findSocketById(io, msg.targetId);
 			const chatroom=await query(`SELECT chatid from chat_room where ((user1id = '${msg.targetId}') and (user2id = '${socket.user_id}')) or ((user2id = '${msg.targetId}') and (user1id = '${socket.user_id}'))`);
-			let today = new Date(); 
-			console.log(today);
 			if (chatroom.length==0){
 				await query(`INSERT INTO chat_room(user1id, user2id) values ('${socket.user_id}', '${msg.targetId}' ) ;`)
 			}
 			if (!msg.expire_time){
 				await query(`INSERT INTO message(senderid, receiverid, content, sendtime, chatid, noticed) SELECT f.user_id, t.user_id, '${msg.message}', '${msg.created_at}', c.chatid, '${msg.noticed}' FROM users f, users t, chat_room c WHERE f.user_id = '${socket.user_id}' and t.user_id = '${msg.targetId}' and ((c.user1id = '${socket.user_id}' and c.user2id = '${msg.targetId}') or (c.user1id = '${msg.targetId}' and c.user2id = '${socket.user_id}'));`)
 			}
-			else{
+			else{	
+				//랑데뷰전송일 때
+				//'${socket.user_id}' : 로그인한사람
+				//'${msg.targetId}' : 받는사람
+				//if (sender가 상대 receiver가 나일 때 ) : 랑데부 메세지인지
+				//	if sender receiver 500m이내
+				//		insert ~~~~~~~~~~~~	
 				await query(`INSERT INTO message(senderid, receiverid, content, sendtime, chatid, expire_time, noticed, durtime) SELECT f.user_id, t.user_id, '${msg.message}', '${msg.created_at}', c.chatid, '${msg.expire_time}', '${msg.noticed}', '${msg.durtime}' FROM users f, users t, chat_room c WHERE f.user_id = '${socket.user_id}' and t.user_id = '${msg.targetId}' and ((c.user1id = '${socket.user_id}' and c.user2id = '${msg.targetId}') or (c.user1id = '${msg.targetId}' and c.user2id = '${socket.user_id}'));`)
 			}
 			
